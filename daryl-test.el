8081b16dc5d16ca82bb584305f88ebc3e5b9546e
Daryl.Xu <ziqiang_xu@yeah.net>
2018-11-01 14:24:43 +0800

    adjust the order of the config

--- daryl-test.el
+++ daryl-test.el
@@ -1,15 +1,15 @@
 (provide 'daryl-test)
 (setq cursor-type 'bar)
 
-;;打开daryl-test.el
+;; 定义一个打开daryl-test.el的函数
 (defun load-daryl-test()
   (interactive)
-  (find-file "~/elisp/daryl-test.el"))
+  (find-file "~/emacs-learning/daryl-test.el"))
 ;; 将load-daryl-test函数绑定到f3按键
 (global-set-key (kbd "<f3>") 'load-daryl-test)
 
 
-;; 对Go语言的支持
+;; 开启对Go语言的支持
 (setq auto-mode-alist
       (append
        '(("\\.go\\'" . go-mode))
--- init-generic.el
+++ init-generic.el
@@ -1,40 +1,19 @@
+;; 本文件在emacs启动时被~/.emacs文件引入，我在这里对emacs进行基本的配置
+
 (provide 'init-generic)
 ;; 使用ELPA镜像
 (require 'melpa)
-;; 确认操作简化
-(fset 'yes-or-no-p 'y-or-n-p)
-;;(setq mouse-yan-at-point t)
 
-;; 开启全局自动补全功能
-(global-company-mode 1)
 
-;; 工具栏
-(tool-bar-mode -1)
-
-;; 菜单栏
-;;(menu-bar-mode -1)
-
-;; 开启行号
-(global-linum-mode 1)
-
-;; 关闭启动帮助页面
-(setq inhibit-splash-screen 1)
-
+;; 备份相关
 ;; 不生成备份文件
 (setq backup-directory-alist (quote (("." . "~/.backups"))))
 
 ;;关闭自动备份
 (setq make-backup-files nil)      
 
-;; 关闭系统的自动缩进
-;; (electric-indent-mode -1)
 
-;;开启历史记录，可以点击菜单栏打开历史文件
-(require 'recentf)
-(recentf-mode 1)
-;;(setq recentf-max-menu-item 10)
-
-;; --- 鼠标样式 ---
+;; --- 鼠标相关 ---
 ;; 关闭光标闪烁
 (blink-cursor-mode -1)
 
@@ -48,8 +27,46 @@
 ;; 高亮鼠标所在行
 (global-hl-line-mode 1)
 
+;; 是否开启行号
+(global-linum-mode 1)
+
+
+;; --- 历史记录 ---
+;;开启历史记录，可以点击菜单栏打开历史文件
+(require 'recentf)
+(recentf-mode 1)
+;; 可显示的历史记录数目
+;;(setq recentf-max-menu-item 10)
+
+
+;; --- 窗口相关 ---
 ;;默认全屏
 ;;(setq initial-frame-alist '((fullscreen . maximized)))
+
+;; 是否显示工具栏
+(tool-bar-mode -1)
+
+;; 是否显示菜单栏
+;;(menu-bar-mode -1)
+
+;; 关闭启动帮助页面
+(setq inhibit-splash-screen 1)
+
+;; --- 其它 ---
+;; 关闭系统的自动缩进
+;; (electric-indent-mode -1)
+
+;; 回答编辑的询问时，用y代表yes
+(fset 'yes-or-no-p 'y-or-n-p)
+
+;; 这个我忘记有什么用了
+;;(setq mouse-yan-at-point t)
+
+;; 开启全局自动补全功能
+(global-company-mode 1)
+
+
+
 
 ;; --- 引入测试代码的文件 ---
 (require 'daryl-test)
