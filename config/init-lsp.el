d5b4e6b51e87d4f7e26d2e7e7db5633e8b1fb322
Daryl.Xu <ziqiang_xu@qq.com>
2021-01-11 02:20:44 +0800

    feat: config for rust

--- config/init-lsp.el
+++ config/init-lsp.el
@@ -7,14 +7,20 @@
 ;; 使用prog-mode-hook则可以自动判断所编辑的文件
 (add-hook 'prog-mode-hook #'lsp)
 ;;(add-hook 'go-mode-hook #'lsp)
-;;(require 'lsp-python)
+;;(require 'init-python)
 ;;(add-hook 'python-mode-hook #'lsp)
 ;;(add-hook 'go-mode-hook #'lsp-go-enable)
 ;;(add-hook 'c++-mode-hook #'lsp)
 
 ;; 开启补全
-(require 'company-lsp)
-(push 'company-lsp company-backends)
+;;(require 'company-lsp)
+;;(push 'company-lsp company-backends)
+
+;; >>> Rust
+(require 'rust-mode)
+(setq lsp-rust-server 'rust-analyzer)
+(require 'rust-mode)
+;; <<<
 
 ;; lsp-ui
 (require 'lsp-ui)
--- melpa.el
+++ melpa.el
@@ -8,8 +8,14 @@
 (when (>= emacs-major-version 24)
      (require 'package)
      (package-initialize)
-     (setq package-archives '(("gnu"   . "http://elpa.emacs-china.org/gnu/")
-		      ("melpa" . "http://elpa.emacs-china.org/melpa/"))))
+     (setq package-archives '(("gnu"  . "https://elpa.gnu.org/packages/")
+;;			      ("melpa" . "http://elpa.emacs-china.org/melpa/"))))
+			      ("melpa" . "http://elpa.emacs-china.org/melpa-stable/"))))
+;;			      ("melpa" . "https://melpa.org/packages/"))))
+;;			      ("melpa" . "https://stable.melpa.org/packages/"))))
+
+;;("gnu"   . "https://elpa.emacs-china.org/gnu/")
+
 
 ;; 注意 elpa.emacs-china.org 是 Emacs China 中文社区在国内搭建的一个 ELPA 镜像
 
